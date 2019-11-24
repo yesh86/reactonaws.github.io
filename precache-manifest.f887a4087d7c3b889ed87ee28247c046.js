2cac0c01607f4831eb8cd4ebf9124730b74373b3
yesh86 <yeshwant86@gmail.com>
2019-11-23 23:53:42 -0500

    Updates

---
+++ precache-manifest.f887a4087d7c3b889ed87ee28247c046.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5a23150f141c2893f6ae21a51c654b6",
+    "url": "/reactapponaws/index.html"
+  },
+  {
+    "revision": "45255fe4ed7837721464",
+    "url": "/reactapponaws/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "d0142264f4bf73deb90f",
+    "url": "/reactapponaws/static/js/2.855da963.chunk.js"
+  },
+  {
+    "revision": "45255fe4ed7837721464",
+    "url": "/reactapponaws/static/js/main.c335ce5b.chunk.js"
+  },
+  {
+    "revision": "17e9eb85794cdb7e9e07",
+    "url": "/reactapponaws/static/js/runtime-main.2212717c.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/reactapponaws/static/media/logo.25bf045c.svg"
+  }
+]);
